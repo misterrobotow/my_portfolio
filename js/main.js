b6a0676408c86d97a6efd1cdadadc0a7f0eb0220
Daniel Gomez <franciscodaniel18888@gmail.com>
2022-05-18 22:40:30 -0500

    New font and new design on home page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 let toggleThemeButton = document.getElementById("toggle-theme");
+
+if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+    document.body.classList.toggle("dark");
+}
+
 toggleThemeButton.addEventListener("click", (event) => {
     document.body.classList.toggle("dark");
 });
