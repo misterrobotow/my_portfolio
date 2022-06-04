7068df2031d8e5d7d9ff9276c2284a3efad2ea1b
Daniel Gomez <franciscodaniel18888@gmail.com>
2022-06-03 23:16:55 -0500

    My heros section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let toggleThemeButton = document.getElementById("toggle-theme");
+const sections = document.querySelectorAll(".section[id]");
+console.log(sections);
 
 if (
     window.matchMedia &&
@@ -10,3 +12,24 @@ if (
 toggleThemeButton.addEventListener("click", (event) => {
     document.body.classList.toggle("dark");
 });
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 100;
+        sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(".menu a[href*=" + sectionId + "]")
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(".menu a[href*=" + sectionId + "]")
+                .classList.remove("active-link");
+        }
+    });
+}
+window.addEventListener("scroll", scrollActive);
